6c3a6e2417f12697a07a5cb71c074254fa89d04f
Gabrielle Oliveira Santana <gaboliveirasantana1@gmail.com>
2026-08-02 21:15:27 -0300

    style: normalize string quotes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portifolio/',
+  base: "/portifolio/",
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    outDir: 'build',
+    outDir: "build",
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
     css: true,
   },
 });
